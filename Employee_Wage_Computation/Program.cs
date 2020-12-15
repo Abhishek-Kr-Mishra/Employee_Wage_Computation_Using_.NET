b6778667889fc5da768a30919d3ddace579040d1
abh224272 <abh224272@gmail.com>
2020-12-15 15:13:20 +0530

    Add: Calculation of One Month Salary

--- Employee_Wage_Computation/Program.cs
+++ Employee_Wage_Computation/Program.cs
@@ -9,26 +9,32 @@ namespace Employee_Wage_Computation
             Console.WriteLine("Welcome to Employee Wage Computation Program");
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
+            int NUM_OF_WORKING_DAYS = 20;
             int EMP_RATE_PER_HOUR = 20;
-            int empHrs, empWage;
+            int empHrs=0, empWage, totalEmpWage=0;
 
             Random random = new Random();
 
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Day= "+day +"  Wage= " + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee Wage = " + empWage);
+            Console.WriteLine("Total Employee Wage= " + totalEmpWage);
         }
     }
 }
