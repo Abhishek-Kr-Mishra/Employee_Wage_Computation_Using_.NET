5018b1d8c32df5fec42b16b89228b3f3bff31bd7
abh224272 <abh224272@gmail.com>
2020-12-15 14:50:53 +0530

    Add: Calculation of Employee Wage using switch case Statement

--- Employee_Wage_Computation/Program.cs
+++ Employee_Wage_Computation/Program.cs
@@ -7,25 +7,25 @@ namespace Employee_Wage_Computation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs, empWage;
 
             Random random = new Random();
 
             int empCheck = random.Next(0, 3);
-            if(empCheck == IS_FULL_TIME)
+            switch (empCheck)
             {
-                empHrs = 8;
-            }
-            else if (empCheck == IS_PART_TIME)
-            {
-                empHrs = 4;
-            }
-            else
-            {
-                empHrs = 0;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee Wage = " + empWage);
