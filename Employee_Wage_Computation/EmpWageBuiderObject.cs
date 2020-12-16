9ff885b7892326647f1e2d35b23f38a0278cf7a5
abh224272 <abh224272@gmail.com>
2020-12-16 13:04:51 +0530

    [Abhishek Mishra] Add: Created a Class to save Total Emp Wage of Each Company

---
+++ Employee_Wage_Computation/EmpWageBuiderObject.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_Wage_Computation
+{
+   
+
+    class EmpWageBuiderObject
+    {
+        const int IS_FULL_TIME = 1;
+        const int IS_PART_TIME = 2;
+
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+
+        public EmpWageBuiderObject(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public int ComputeEmpWage()
+        {
+            int empHrs = 0, empWage, totalEmpHours = 0, totalWorkingDays = 0;
+            Random random = new Random();
+
+            while (totalWorkingDays < numOfWorkingDays && totalEmpHours < maxHoursPerMonth)
+            {
+                totalWorkingDays++;
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        totalEmpHours += 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        totalEmpHours += 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * empRatePerHour;
+                totalEmpWage += empWage;
+                Console.WriteLine("Day= " + totalWorkingDays + "  Wage= " + empHrs);
+            }
+            Console.WriteLine("Total Emp Wage for " + company + " is " + totalEmpWage);
+            return totalEmpWage;
+        }
+        public string toString()
+        {
+            return "Total Emp Wage for company" + this.company + " is " + this.totalEmpWage;
+        }
+    }
+}
--- Employee_Wage_Computation/Program.cs
+++ Employee_Wage_Computation/Program.cs
@@ -5,46 +5,18 @@ namespace Employee_Wage_Computation
 {
     class Program
     {
-        const int IS_FULL_TIME = 1;
-        const int IS_PART_TIME = 2;
         
-
-        public static int ComputeEmpWage(string company, int empratePerHour, int numOfWorkingDays, int maxHoursPerMonth)
-        {
-            int empHrs = 0, empWage, totalEmpWage = 0, totalEmpHours = 0, totalWorkingDays = 0;
-            Random random = new Random();
-
-            while (totalWorkingDays < numOfWorkingDays && totalEmpHours < maxHoursPerMonth)
-            {
-                totalWorkingDays++;
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        totalEmpHours += 8;
-                        break;
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        totalEmpHours += 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                empWage = empHrs * empratePerHour;
-                totalEmpWage += empWage;
-                Console.WriteLine("Day= " + totalWorkingDays+"  Wage= " + empHrs);
-            }
-            Console.WriteLine("Total Emp Wage for " + company + " is " + totalEmpWage);
-            return totalEmpWage;
-        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            ComputeEmpWage("D-mart", 20, 20, 50);
-            Console.WriteLine();
-            ComputeEmpWage("Big-Bazar", 20, 10, 50);
+            EmpWageBuiderObject dmart = new EmpWageBuiderObject("D-Mart", 20, 50, 10);
+            EmpWageBuiderObject bigBazar = new EmpWageBuiderObject("Big-Bazar", 20, 80, 20);
+            dmart.ComputeEmpWage();
+            Console.WriteLine(dmart.toString());
+
+            bigBazar.ComputeEmpWage();
+            Console.WriteLine(bigBazar.toString());
+
         }
     }
 }
