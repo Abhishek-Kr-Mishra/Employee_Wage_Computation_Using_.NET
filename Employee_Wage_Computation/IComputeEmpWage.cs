0706fe07a68f1a522a46752ee97b225f6ef8d198
abh224272 <abh224272@gmail.com>
2020-12-18 12:07:49 +0530

    [Abhishek Mishra] Add: Interface to use API Approach

---
+++ Employee_Wage_Computation/IComputeEmpWage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_Wage_Computation
+{
+    interface IComputeEmpWage
+    {
+        public void AddCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+    }
+}
